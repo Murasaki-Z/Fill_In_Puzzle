429ded518bf4cb05095729b67e6cf08b98adc23b
Abhishek Anand <anand.abhishek961@gmail.com>
2020-05-31 03:51:06 +1000

    Create v5.pl

---
+++ v5.pl
@@ -0,0 +1,115 @@
+
+:- ensure_loaded(library(clpfd)).
+:- set_prolog_flag(answer_write_options,
+                   [ quoted(true),
+                     portray(true),
+                     spacing(next_argument)
+                   ]).
+
+puzzle_solution(Puzzle, WordList) :-
+	squareUtil(Puzzle,Square),
+	fillUtil(Square, WordList).
+
+
+
+
+squareUtil(Puzzle,Square):-
+	squareHelper(Puzzle,S1),
+	transpose(Puzzle,TPuzzle),
+	squareHelper(TPuzzle,S2),
+	append(S1,S2,Square).
+
+
+
+same_chars([],_).
+same_chars([Start|Last],[StartList|LastLists]):-
+	same_chars(Last,LastLists).
+
+
+canJoin(S1, W1) :-
+	same_length(S1,W1),
+	same_chars(S1,W1).
+
+join(X2, X2).
+
+getsquares([], Currentsquare, Square) :-
+    length(Currentsquare, N),
+    (   N > 1
+    ->  Square = [Currentsquare]
+    ;   Square = []
+    ).
+
+
+getsquares([Var|Vars], Currentsquare, Square) :-
+    (   Var \== '#'
+    ->  append(Currentsquare, [Var], Currentsquare1),
+        getsquares(Vars, Currentsquare1, Square)
+    ;   length(Currentsquare, N),
+        (   N > 1
+        ->  Square = [Currentsquare|S1]
+        ;   Square = S1
+        ),
+        getsquares(Vars, [], S1)
+    ).
+
+
+squareHelper([], []).
+squareHelper([Row|Rows], Square) :-
+    getsquares(Row, [], S1),
+    squareHelper(Rows, S2),
+    append(S1, S2, Square).
+
+
+
+fillUtil([], _).
+fillUtil([Square|S2], WordList) :-
+	
+	combinationCheck(Square, WordList, 0, Matches),
+
+
+	comboUtil(S2, WordList, Matches, Square, BestMatch, [], New),
+	
+	filler(BestMatch, WordList, [], NewWordList),
+
+	
+	fillUtil(New, NewWordList).
+
+filler(Square, [Word|WordList], NewWordListIn, NewWordListOut) :-
+	
+	join(Square, Word),
+	append(WordList, NewWordListIn, NewWordListOut);
+
+	append(NewWordListIn, [Word], NewWordListTemp),
+	filler(Square, WordList, NewWordListTemp, NewWordListOut).
+
+
+
+comboUtil([], _, Least, Current, Current,Input, Input) :-
+	Least > 0.
+comboUtil([Square|OtherSquares], WordList, Least, Current,
+	BestMatch, Input, Output) :-
+	combinationCheck(Square, WordList, 0, Total),
+	(	Total < Least
+	->	
+		append(Input, [Current], InputTemp),
+		comboUtil(OtherSquares, WordList, Total, Square, BestMatch,
+			InputTemp, Output)
+
+	;	
+		append(Input, [Square], InputTemp),
+		comboUtil(OtherSquares, WordList, Least, Current,
+			BestMatch, InputTemp, Output)
+	).
+
+
+combinationCheck(_, [], CurrentMatches, CurrentMatches).
+combinationCheck(Square, [Word|OtherWords], CurrentMatches, Total) :-
+	(	canJoin(Square, Word)
+	->	combinationCheck(Square, OtherWords, CurrentMatches+1, Total)
+	;	combinationCheck(Square, OtherWords, CurrentMatches, Total)
+	).
+
+
+
+
+
